ecf5cf2b98090d7aec3d7050bb3b69a170cd2212
ChrisMiuchiz <chris000005@gmail.com>
2021-07-18 17:08:31 -0400

    Add serlization to Service

--- src/password/manager/Model/Service.java
+++ src/password/manager/Model/Service.java
@@ -1,10 +1,19 @@
 package password.manager.Model;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+import com.google.gson.annotations.Expose;
 
 public class Service implements Serializable {
+    @Expose
     private String serviceName;
+    @Expose
     private String userName;
+    @Expose
     private String password;
+    @Expose
     private String notes;
     
     public Service() {
@@ -48,12 +57,17 @@ public class Service implements Serializable {
     
     @Override
     public void fromJSON(String json) {
-        throw new UnsupportedOperationException("Not supported yet.");
+        JsonParser jsonParser = new JsonParser();
+        JsonObject jsonObject = jsonParser.parse(json).getAsJsonObject();
+        password = jsonObject.get("password").getAsString();
+        notes = jsonObject.get("notes").getAsString();
+        userName = jsonObject.get("userName").getAsString();
+        serviceName = jsonObject.get("serviceName").getAsString();
     }
 
     @Override
     public String toJSON() {
-        throw new UnsupportedOperationException("Not supported yet.");
+        Gson gson = new GsonBuilder().excludeFieldsWithoutExposeAnnotation().create();
+        return gson.toJson(this);
     }
-
 }
